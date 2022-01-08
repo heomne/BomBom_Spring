564d275c8f26ad5a37c5e963a57acbcd74ebfccd
LG <LG@USER>
2022-01-08 15:09:43 +0900

    저장

--- bombom/src/main/java/com/bombom/controller/PremiereController.java
+++ bombom/src/main/java/com/bombom/controller/PremiereController.java
@@ -19,8 +19,6 @@ import com.bombom.model.PremiereDTO;
 @Controller
 public class PremiereController {
 	
-	final String uri = "/upload";
-	
 	@Autowired
 	private PremiereDAO dao;
 	
@@ -31,32 +29,32 @@ public class PremiereController {
 	}
 	
 	// 시사회 게시판 글 작성 완료하는 비즈니스 로직
+	//MultipartHttpServletRequest : "multipart/form-data"로 전달받은  입력파일과 입력값을 처리하는 인스턴스
 	@RequestMapping(value = "/premiere_write.do", method = RequestMethod.POST)
 	public String premiere_insert(PremiereDTO dto, 
-			MultipartHttpServletRequest mRequest) 
+			MultipartHttpServletRequest request) 
 					throws IllegalStateException, IOException {
 		
-		MultipartFile thumbnail = mRequest.getFile("thumbnail");
+		// 화면단에서 전달되는 파일 받을 객체
+		MultipartFile thumbnail = request.getFile("thumbnail");
 		
-		// 파일 저장
-		File file = getFile(mRequest, uri, thumbnail.getOriginalFilename());
+		// 파일 저장할 경로 지정
+		String filePath = 
+				request.getSession().getServletContext().getRealPath("resources/upload/premiere/");	
 		
+		System.out.println("시사회 게시판 썸네일 파일경로 > " + filePath);
+		
+		// 파일을 업로드하기 위한 파일 객체 생성
+		File file = new File(filePath, thumbnail.getOriginalFilename());
+		
+		// 파일 업로드
+		// transferTo : 업로드요청으로 전달받은 파일을 위에서 설정한 특정 경로에 특정 파일명으로 저장
 		thumbnail.transferTo(file);
 		
 		dto.setPremiere_thumbnail(file.getName());
+		
 		int result = dao.insertBoard(dto);
 		
 		return "/user/user_premiere";
 	}
-	
-	// File 객체를 생성해서 반환 
-	private File getFile(HttpServletRequest request, String uri, String fileName) {
-		
-		String rpath = request.getSession().getServletContext().getRealPath(uri);
-		
-		File newFile = new File(rpath, fileName); 
-		
-		return newFile;
-	}
-	
 }
